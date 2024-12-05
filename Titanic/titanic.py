d439c6be4ea20610e4f7ea1f24e0a36f81737c1f
mayara-canaver <mayaracanaver@hotmail.com>
2024-12-05 19:44:39 -0300

    just correcting a syntaxe ocd

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import recall_score
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score, accuracy_score
 
 df = pd.read_csv("train.csv")
 df = df.dropna()
