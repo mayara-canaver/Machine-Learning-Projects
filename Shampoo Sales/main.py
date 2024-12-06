54acee33ce1f592941c619d0e08365e73c61b3d3
mayara-canaver <mayaracanaver@hotmail.com>
2024-12-06 00:29:36 -0300

    updating and fixing parameters from arima model

--- Shampoo Sales/main.py
+++ Shampoo Sales/main.py
@@ -1,98 +1,54 @@
 import pandas as pd
-import csv
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-from scipy.stats import shapiro
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import plot_acf
-from statsmodels.tsa.seasonal import seasonal_decompose
-from pmdarima.arima import auto_arima
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.stattools import adfuller
 
+df = pd.read_csv("shampoo_sales.csv")
 
-def trata_coluna(data):
-    data.columns = [x.upper() for x in data.columns]
+df["Month"] = "0" + df["Month"]
+df["Month"] = pd.to_datetime(df["Month"], format="%y-%m")
 
-    for coluna in data:
-        data[coluna] = data[coluna].str.strip()
-        data[coluna] = data[coluna].str.replace('"', '')
-        data[coluna] = data[coluna].str.upper()
+X = df["Sales"].values
 
-
-d_shampoo = "C:/Users/Mayara Lopes/Desktop/shampoo_sales.csv"
-
-shampoo = pd.read_csv(d_shampoo, sep=",", encoding="UTF-8", dtype=str, error_bad_lines=False, quoting=csv.QUOTE_NONE)
-
-trata_coluna(shampoo)
-
-shampoo = shampoo.rename({'"MONTH"': "MONTH", '"SALES"': "SALES"}, axis=1)
-
-shampoo["SALES"] = pd.to_numeric(shampoo["SALES"])
-
-shampoo["MONTH"] = "0" + shampoo["MONTH"]
-shampoo["MONTH"] = pd.to_datetime(shampoo["MONTH"], format="%y-%m", yearfirst=True)
-
-#perform Shapiro-Wilk test
-shapiro(shampoo["SALES"])
-
-sns.lineplot(data=shampoo, x="MONTH", y="SALES")
-
-
-plot_acf(shampoo["SALES"].values.astype("float32"))
-
-X = shampoo["SALES"].values
 result = adfuller(X)
+
 print('ADF Statistic: %f' % result[0])
 print('p-value: %f' % result[1])
 print('Critical Values:')
+
 for key, value in result[4].items():
 	print('\t%s: %.3f' % (key, value))
+	
+df['Difference'] = df['Sales'].diff(periods=1)
 
-resultado = seasonal_decompose(shampoo["SALES"], model='additive', period=5)
-resultado.plot()
+df.dropna(inplace=True)
 
-rolling = shampoo.rolling(window=3)
-rolling_mean = rolling.mean()
-shampoo["MEDIAS MOVEIS"] = rolling_mean
-sns.lineplot(data=shampoo, x="MONTH", y="SALES")
-sns.lineplot(data=shampoo, x="MONTH", y="MEDIAS MOVEIS")
+plot_acf(df["Difference"])
 
+df['Moving Average'] = df['Difference'].rolling(window=3).mean()
 
-def difference(dataset, interval):
-	diff = list()
+sns.lineplot(data=df, x='Month', y='Difference', label='Difference')
+sns.lineplot(data=df, x='Month', y='Moving Average', label='Moving Average')
+plt.legend()
+plt.show()
 
-	for i in range(interval, len(dataset)):
-		value = dataset[i] - dataset[i - interval]
-		diff.append(value)
+df_train = df[:24]
+df_test = df[24:]
 
-	return pd.Series(diff)
+model = ARIMA(df_train["Difference"], order=[1, 1, 3])
+predict = model.fit().forecast(12)
+df_test["Predict"] = predict
 
+sns.lineplot(data=df_train, x="Month", y="Difference", label="Actual Sales")
+sns.lineplot(data=df_test, x="Month", y="Predict", label="Predicted Sales")
+sns.lineplot(data=df_test, x="Month", y="Difference", label="Actual Sales (Test Set)")
+plt.legend()
+plt.show()
 
-diferenciacao = difference(shampoo["SALES"], 1)
-shampoo["DIFERENCIACAO"] = diferenciacao
-sns.lineplot(data=shampoo, x="MONTH", y="DIFERENCIACAO")
+result = mean_squared_error(df_test["Difference"], df_test["Predict"])
 
-shampoo_treino = shampoo[:24]
-shampoo_teste = shampoo[24:]
-
-modelo = ARIMA(shampoo_treino["SALES"], order=[2, 1, 3])
-resultado = modelo.fit().forecast(12)
-shampoo_teste["PREVISAO"] = resultado
-
-sns.lineplot(data=shampoo_treino, x="MONTH", y="SALES")
-sns.lineplot(data=shampoo_teste, x="MONTH", y="PREVISAO")
-sns.lineplot(data=shampoo_teste, x="MONTH", y="SALES")
-
-modelo_auto_arima = auto_arima(shampoo_treino["SALES"].values, error_action="ignore", trace=True)
-
-previsao_auto_arima = modelo_auto_arima.predict(12)
-shampoo_teste["PREVISAO_AUTO_ARIMA"] = previsao_auto_arima
-sns.lineplot(data=shampoo_teste, x="MONTH", y="PREVISAO_AUTO_ARIMA")
-sns.lineplot(data=shampoo_teste, x="MONTH", y="SALES")
-
-resultado_auto_arima = mean_squared_error(shampoo_teste["SALES"], shampoo_teste["PREVISAO_AUTO_ARIMA"])
-resultado_arima = mean_squared_error(shampoo_teste["SALES"], shampoo_teste["PREVISAO"])
-
-print(f"AUTO ARIMA = {resultado_auto_arima}")
-print(f"ARIMA = {resultado_arima}")
+print(f"ARIMA = {result}")
